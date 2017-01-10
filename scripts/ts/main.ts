bb604995a8552a7b7fa4eeac291a89e8770cf6c4
Unknown <blanksparrow@outlook.com>
2017-01-11 01:30:22 +1300

    Masking

--- scripts/ts/main.ts
+++ scripts/ts/main.ts
@@ -7,3 +7,19 @@ for (var i = 0; i < circles.length; i++) {
   circles[i].setAttribute("onclick","this.classList.toggle('circle-active');");
   //circles[i].addEventListener("click", () => {circles[i].classList.toggle("circle-active")});
 }
+
+var top_wrapper = document.getElementsByClassName("top-wrapper")[0];
+var bottom_wrapper = document.getElementsByClassName("bottom-wrapper")[0];
+
+var top_left = document.getElementsByClassName("top-left")[0];
+var top_right = document.getElementsByClassName("top-right")[0];
+var bottom_left = document.getElementsByClassName("bottom-left")[0];
+var bottom_right = document.getElementsByClassName("bottom-right")[0];
+
+//top_wrapper.setAttribute("onclick", "top_wrapper.classList.toggle('wrapper-active');bottom_wrapper.classList.toggle('wrapper-hidden');");
+//bottom_wrapper.setAttribute("onclick", "top_wrapper.classList.toggle('wrapper-hidden');bottom_wrapper.classList.toggle('wrapper-active');");
+
+top_left.setAttribute("onclick", "top_left.classList.toggle('circle-mask');top_left.classList.toggle('circle-active');top_right.classList.toggle('circle-hidden');top_wrapper.classList.toggle('wrapper-hidden');bottom_wrapper.classList.toggle('wrapper-active');");
+top_right.setAttribute("onclick", "top_right.classList.toggle('circle-mask');top_right.classList.toggle('circle-active');top_left.classList.toggle('circle-hidden');top_wrapper.classList.toggle('wrapper-hidden');bottom_wrapper.classList.toggle('wrapper-active');");
+bottom_left.setAttribute("onclick", "bottom_left.classList.toggle('circle-mask');bottom_left.classList.toggle('circle-active');bottom_right.classList.toggle('circle-hidden');top_wrapper.classList.toggle('wrapper-active');bottom_wrapper.classList.toggle('wrapper-hidden');");
+bottom_right.setAttribute("onclick", "bottom_right.classList.toggle('circle-mask');bottom_right.classList.toggle('circle-active');bottom_left.classList.toggle('circle-hidden');top_wrapper.classList.toggle('wrapper-active');bottom_wrapper.classList.toggle('wrapper-hidden');");

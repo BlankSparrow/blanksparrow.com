d71b55f1aead84140caeb4cc7e38c45c3c8d9d16
Unknown <blanksparrow@outlook.com>
2017-01-15 22:44:19 +1300

    Updates

--- scripts/ts/main.ts
+++ scripts/ts/main.ts
@@ -19,7 +19,25 @@ var bottom_right = document.getElementsByClassName("bottom-right")[0];
 //top_wrapper.setAttribute("onclick", "top_wrapper.classList.toggle('wrapper-active');bottom_wrapper.classList.toggle('wrapper-hidden');");
 //bottom_wrapper.setAttribute("onclick", "top_wrapper.classList.toggle('wrapper-hidden');bottom_wrapper.classList.toggle('wrapper-active');");
 
-top_left.setAttribute("onclick", "top_left.classList.toggle('circle-mask');top_left.classList.toggle('circle-active');top_right.classList.toggle('circle-hidden');top_wrapper.classList.toggle('wrapper-hidden');bottom_wrapper.classList.toggle('wrapper-active');");
-top_right.setAttribute("onclick", "top_right.classList.toggle('circle-mask');top_right.classList.toggle('circle-active');top_left.classList.toggle('circle-hidden');top_wrapper.classList.toggle('wrapper-hidden');bottom_wrapper.classList.toggle('wrapper-active');");
-bottom_left.setAttribute("onclick", "bottom_left.classList.toggle('circle-mask');bottom_left.classList.toggle('circle-active');bottom_right.classList.toggle('circle-hidden');top_wrapper.classList.toggle('wrapper-active');bottom_wrapper.classList.toggle('wrapper-hidden');");
-bottom_right.setAttribute("onclick", "bottom_right.classList.toggle('circle-mask');bottom_right.classList.toggle('circle-active');bottom_left.classList.toggle('circle-hidden');top_wrapper.classList.toggle('wrapper-active');bottom_wrapper.classList.toggle('wrapper-hidden');");
+top_left.setAttribute("onclick", "top_left_click()");
+top_right.setAttribute("onclick", "top_right_click()");
+bottom_left.setAttribute("onclick", "bottom_left.classList.toggle('circle-mask'); top_right.classList.toggle('circle-hidden');top_left.classList.toggle('circle-hidden'); bottom_left.classList.toggle('circle-active');bottom_right.classList.toggle('circle-hidden');top_wrapper.classList.toggle('wrapper-active');bottom_wrapper.classList.toggle('wrapper-hidden');");
+bottom_right.setAttribute("onclick", "bottom_right.classList.toggle('circle-mask'); top_right.classList.toggle('circle-hidden');top_left.classList.toggle('circle-hidden'); bottom_right.classList.toggle('circle-active');bottom_left.classList.toggle('circle-hidden');top_wrapper.classList.toggle('wrapper-active');bottom_wrapper.classList.toggle('wrapper-hidden');");
+
+function top_left_click(){
+  top_left.classList.toggle('circle-active');
+  top_right.classList.toggle('circle-hidden'); 
+  bottom_left.classList.toggle('circle-hidden');
+  bottom_right.classList.toggle('circle-hidden'); 
+  top_wrapper.classList.toggle('wrapper-hidden');
+  bottom_wrapper.classList.toggle('wrapper-active');
+}
+
+function top_right_click(){
+  top_left.classList.toggle('circle-hidden');
+  top_right.classList.toggle('circle-active'); 
+  bottom_left.classList.toggle('circle-hidden');
+  bottom_right.classList.toggle('circle-hidden'); 
+  top_wrapper.classList.toggle('wrapper-hidden');
+  bottom_wrapper.classList.toggle('wrapper-active');
+}

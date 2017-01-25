d5295856bd5b0ae3082d9c5e7634fc88a50298fa
Unknown <blanksparrow@outlook.com>
2017-01-25 19:11:42 +1300

    Circle tests

--- scripts/ts/main.ts
+++ scripts/ts/main.ts
@@ -13,10 +13,10 @@ var center = document.getElementsByClassName("center")[0];
 var top_wrapper = document.getElementsByClassName("top-wrapper")[0];
 var bottom_wrapper = document.getElementsByClassName("bottom-wrapper")[0];
 
-var top_left = document.getElementsByClassName("top-left")[0];
-var top_right = document.getElementsByClassName("top-right")[0];
-var bottom_left = document.getElementsByClassName("bottom-left")[0];
-var bottom_right = document.getElementsByClassName("bottom-right")[0];
+var top_left = document.getElementsByClassName("top-left-align")[0];
+var top_right = document.getElementsByClassName("top-right-align")[0];
+var bottom_left = document.getElementsByClassName("bottom-left-align")[0];
+var bottom_right = document.getElementsByClassName("bottom-right-align")[0];
 
 top_left.setAttribute("onclick", "top_left_click()");
 top_right.setAttribute("onclick", "top_right_click()");
@@ -25,41 +25,41 @@ bottom_right.setAttribute("onclick", "bottom_right_click()");
 
 function top_left_click(){
   center.classList.toggle('center-active');
-  top_left.classList.toggle('circle-active');
-  top_right.classList.toggle('circle-hidden'); 
-  bottom_left.classList.toggle('circle-hidden');
-  bottom_right.classList.toggle('circle-hidden'); 
+  top_left.classList.toggle('top-left-active');
+  top_right.classList.toggle('top-right-hidden'); 
+  bottom_left.classList.toggle('bottom-left-hidden');
+  bottom_right.classList.toggle('bottom-right-hidden'); 
   top_wrapper.classList.toggle('wrapper-active');
-  bottom_wrapper.classList.toggle('wrapper-hidden');
+  bottom_wrapper.classList.toggle('wrapper-active');
 }
 
 function top_right_click(){
   center.classList.toggle('center-active');
-  top_left.classList.toggle('circle-hidden');
-  top_right.classList.toggle('circle-active'); 
-  bottom_left.classList.toggle('circle-hidden');
-  bottom_right.classList.toggle('circle-hidden'); 
+  top_left.classList.toggle('top-left-hidden');
+  top_right.classList.toggle('top-right-active'); 
+  bottom_left.classList.toggle('bottom-left-hidden');
+  bottom_right.classList.toggle('bottom-right-hidden'); 
   top_wrapper.classList.toggle('wrapper-active');
-  bottom_wrapper.classList.toggle('wrapper-hidden');
+  bottom_wrapper.classList.toggle('wrapper-active');
 }
 
 
 function bottom_left_click(){
   center.classList.toggle('center-active');
-  top_left.classList.toggle('circle-hidden');
-  top_right.classList.toggle('circle-hidden'); 
-  bottom_left.classList.toggle('circle-active');
-  bottom_right.classList.toggle('circle-hidden'); 
+  top_left.classList.toggle('top-left-hidden');
+  top_right.classList.toggle('top-right-hidden'); 
+  bottom_left.classList.toggle('bottom-left-active');
+  bottom_right.classList.toggle('bottom-right-hidden'); 
   top_wrapper.classList.toggle('wrapper-hidden');
   bottom_wrapper.classList.toggle('wrapper-active');
 }
 
 function bottom_right_click(){
   center.classList.toggle('center-active');
-  top_left.classList.toggle('circle-hidden');
-  top_right.classList.toggle('circle-hidden'); 
-  bottom_left.classList.toggle('circle-hidden');
-  bottom_right.classList.toggle('circle-active'); 
+  top_left.classList.toggle('top-left-hidden');
+  top_right.classList.toggle('top-right-hidden'); 
+  bottom_left.classList.toggle('bottom-left-hidden');
+  bottom_right.classList.toggle('bottom-right-active'); 
   top_wrapper.classList.toggle('wrapper-hidden');
   bottom_wrapper.classList.toggle('wrapper-active');
 }

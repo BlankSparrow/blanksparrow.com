0ba9bd23913bdd46b52bb88e433b59029de67abb
Jeremy Craig <blanksparrow@outlook.com>
2017-01-28 19:32:03 +1300

    Working with vs?

---
+++ gulpfile.js
@@ -0,0 +1,89 @@
+﻿const gulp = require('gulp');
+const cssnano = require('gulp-cssnano');
+const sass = require('gulp-sass');
+const nodemon = require('gulp-nodemon');
+const ts = require('gulp-typescript');
+const uglify = require('gulp-uglify');
+const webp = require('gulp-webp');
+const imagemin = require('gulp-imagemin');
+//const responsive 		= require('gulp-responsive');
+const browserSync = require('browser-sync');
+
+
+gulp.task('default', ['watch'], function () {
+});
+
+gulp.task('watch', ['sass', 'typescript', 'images', 'fonts', 'svg', 'browser-sync', 'nodemon'], function () {
+    gulp.watch('styles/scss/**/*.scss', ['sass']);
+    gulp.watch('scripts/ts/**/*.ts', ['typescript']);
+    gulp.watch('views/images/*', ['images']);
+    gulp.watch('views/svg/*', ['svg']);
+    gulp.watch('views/fonts/*', ['fonts']);
+    // Other watchers
+})
+
+gulp.task('images', function () {
+    gulp.src('assets/images/*')
+        .pipe(imagemin())
+        .pipe(webp())
+        .pipe(gulp.dest('public/images'))
+});
+
+gulp.task('svg', function () {
+    gulp.src('assets/svg/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('public/svg'))
+});
+
+gulp.task('fonts', function () {
+    gulp.src('assets/fonts/*')
+        .pipe(gulp.dest('public/fonts'))
+});
+
+gulp.task('typescript', function () {
+    return gulp.src('scripts/ts/**/*.ts')
+        .pipe(ts({
+            noImplicitAny: true,
+            out: 'main.js'
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('public/js'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('sass', function () {
+    return gulp.src('styles/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+        .pipe(sass({ includePaths: ['./views/scss'] }).on('error', sass.logError))
+        .pipe(cssnano({ autoprefixer: { add: true } }))
+        .pipe(gulp.dest('public/css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+gulp.task('browser-sync', function () {
+    browserSync.init(null, {
+        proxy: "http://localhost:1337",
+        files: ["views/**/*.*"],
+        browser: "chrome",
+        port: 3000,
+    });
+});
+
+gulp.task('nodemon', function (cb) {
+
+    var started = false;
+
+    return nodemon({
+        script: 'server.js'
+    }).on('start', function () {
+        // to avoid nodemon being started multiple times
+        // thanks @matthisk
+        if (!started) {
+            cb();
+            started = true;
+        }
+    });
+});
---
+++ server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var port = process.env.port || 1337;
+
+// server.js
+// load the things we need
+var express = require('express');
+var app = express();
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// add public directories to app
+['css', 'js', 'images', 'fonts'].forEach(function (public_dir) {
+    app.use("/" + public_dir, express.static(__dirname + "/public/" + public_dir));
+});
+
+// use res.render to load up an ejs view file
+
+// index page 
+app.get('/', function (req, res) {
+    res.render('pages/index');
+});
+
+// about page 
+app.get('/about', function (req, res) {
+    res.render('pages/about');
+});
+
+// mask page 
+app.get('/mask', function (req, res) {
+    res.render('pages/mask');
+});
+
+app.listen(1337);
+console.log('1337 is the magic port');
